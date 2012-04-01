9ab2e104a7657d95fc9e8c6815163d0e9439d0ab
Kevin Bridges <kevin@cyberswat.com>
2012-04-01 14:10:55 -0400

    Running puppet apply twice

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -e "deb http://apt.puppetlabs.com/ubuntu lucid main\ndeb-src http://apt.puppetlabs.com/ubuntu lucid main\ndeb http://ppa.launchpad.net/sun-java-community-team/sun-java6/ubuntu lucid main\ndeb-src http://ppa.launchpad.net/sun-java-community-team/sun-java6/ubuntu lucid main" >> /etc/apt/sources.list.d/acquia-devops.list && apt-key adv --keyserver keyserver.ubuntu.com --recv 4BD6EC30 && apt-key adv --keyserver keyserver.ubuntu.com --recv 3EBCE749 && apt-get update && apt-get install puppet -y
-puppet apply /etc/puppet/manifests/site.pp
+puppet apply /etc/puppet/manifests/site.pp && puppet apply /etc/puppet/manifests/site.pp
