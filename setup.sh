617bfe77a71b2d3a541ee0e89d696326e463ed26
Kevin Bridges <kevin@cyberswat.com>
2012-08-04 07:21:17 -0600

    Modifying puppet labs repos to account for their decision t move them

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install the keys for the puppetlabs repo and the sun ppa.  Install puppet.
-echo -e "deb http://apt.puppetlabs.com/ubuntu lucid main\ndeb-src http://apt.puppetlabs.com/ubuntu lucid main\ndeb http://ppa.launchpad.net/sun-java-community-team/sun-java6/ubuntu lucid main\ndeb-src http://ppa.launchpad.net/sun-java-community-team/sun-java6/ubuntu lucid main" >> /etc/apt/sources.list.d/acquia-devops.list && apt-key adv --keyserver keyserver.ubuntu.com --recv 4BD6EC30 && apt-key adv --keyserver keyserver.ubuntu.com --recv 3EBCE749 && apt-get update && apt-get install puppet -y
+echo -e "deb http://apt.puppetlabs.com/ lucid main\ndeb-src http://apt.puppetlabs.com/ lucid main\ndeb http://ppa.launchpad.net/sun-java-community-team/sun-java6/ubuntu lucid main\ndeb-src http://ppa.launchpad.net/sun-java-community-team/sun-java6/ubuntu lucid main" >> /etc/apt/sources.list.d/acquia-devops.list && apt-key adv --keyserver keyserver.ubuntu.com --recv 4BD6EC30 && apt-key adv --keyserver keyserver.ubuntu.com --recv 3EBCE749 && apt-get update && apt-get install puppet -y
 
 # Run puppet apply vs puppetd because this is a puppetmaster-less installation.
 puppet apply /etc/puppet/manifests/devops.pp && puppet apply /etc/puppet/manifests/devops.pp
